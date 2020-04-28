02a113f7e2e5fae892a4f05264fea211cb57264b
RomanBerdichevskii <r378ut@gmail.com>
2020-04-28 22:33:35 +0200

    chore: add httppoison for mocked_server

--- mix.exs
+++ mix.exs
@@ -14,13 +14,23 @@ defmodule Elidactyl.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {Elidactyl.Application, [env: Mix.env]},
+      applications: applications(Mix.env),
       extra_applications: [:logger]
     ]
   end
 
+  defp applications(:test), do: applications(:default) ++ [:cowboy, :plug]
+  defp applications(_),     do: [:logger, :httpoison]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:jason, "~> 1.0"},
+      {:poison, "~> 3.1"},
+      {:httpoison, "~> 1.6"},
+      {:plug_cowboy, "~> 2.0"},
+      {:observer_cli, "~> 1.5"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
