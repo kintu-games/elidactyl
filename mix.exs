088092a64e0a9fb586ad6b56e2bb7307876da777
Roman Berdichevskii <r378ut@gmail.com>
2020-05-27 10:09:07 +0200

    chore: add ex_doc dependency for hex.pm

--- mix.exs
+++ mix.exs
@@ -32,13 +32,14 @@ defmodule Elidactyl.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:jason, "~> 1.0"},
       {:poison, "~> 3.1"},
       {:httpoison, "~> 1.6"},
       {:plug_cowboy, "~> 2.0"},
       {:observer_cli, "~> 1.5"},
-      {:ecto_sql, ">= 0.0.0"},
+      {:ecto_sql, ">= 0.0.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
