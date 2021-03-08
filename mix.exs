9ad006a05316ea7e8017b572d47e07bfb43fe1c4
Roman Berdichevskii <roman.berdichevskii@bettyblocks.com>
2021-03-09 00:08:40 +0100

    chore: bump package to 0.3.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Elidactyl.MixProject do
   def project do
     [
       app: :elidactyl,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env),
       description: "Elixir API for Pterodactyl game panel",
