8878f7d28f780daecf1ca9bc7a8af7ad865d1c8c
Alexey Ovchinnikov <alexey.ovchinnikov@yandex.ru>
2021-03-11 18:44:38 +0300

    feat: update server startup ready

---
+++ test/schemas/server/update_build_info_params_test.exs
@@ -0,0 +1,102 @@
+defmodule Elidactyl.Schemas.Server.UpdateBuildInfoParamsTest do
+  use ExUnit.Case
+  use Elidactyl.ChangesetCase
+
+  alias Elidactyl.Schemas.Server.UpdateBuildInfoParams
+  import UpdateBuildInfoParams
+
+  @valid %{
+    allocation: 1,
+    memory: 512,
+    swap: 0,
+    disk: 200,
+    io: 500,
+    cpu: 0,
+    threads: nil,
+    feature_limits: %{databases: 5, allocations: 5, backups: 2},
+  }
+
+  def build_changeset(params \\ %{}) do
+    changeset(%UpdateBuildInfoParams{}, Map.merge(@valid, params))
+  end
+
+  describe "changeset/2" do
+    test "allows valid params" do
+      build_changeset() |> assert_valid()
+    end
+
+    test "validates that mandatory fields are present" do
+      changeset = changeset(%UpdateBuildInfoParams{}, %{})
+      assert_invalid(changeset, :allocation, error_message == "can't be blank")
+      assert_invalid(changeset, :memory, error_message == "can't be blank")
+      assert_invalid(changeset, :swap, error_message == "can't be blank")
+      assert_invalid(changeset, :io, error_message == "can't be blank")
+      assert_invalid(changeset, :cpu, error_message == "can't be blank")
+      assert_invalid(changeset, :disk, error_message == "can't be blank")
+      assert_invalid(changeset, :feature_limits, error_message == "can't be blank")
+    end
+
+    test "validates fields types" do
+      %{allocation: "a"} |> build_changeset() |> assert_invalid(:allocation)
+      %{memory: "a"} |> build_changeset() |> assert_invalid(:memory)
+      %{swap: "a"} |> build_changeset() |> assert_invalid(:swap)
+      %{disk: "a"} |> build_changeset() |> assert_invalid(:disk)
+      %{io: "a"} |> build_changeset() |> assert_invalid(:io)
+      %{cpu: "a"} |> build_changeset() |> assert_invalid(:cpu)
+      %{threads: true} |> build_changeset() |> assert_invalid(:threads)
+      %{feature_limits: "a"} |> build_changeset() |> assert_invalid(:feature_limits)
+    end
+
+    test "allows nil, integer, string, list and range threads" do
+      %{threads: nil} |> build_changeset() |> assert_valid(:threads)
+      %{threads: 1} |> build_changeset() |> assert_valid(:threads)
+      %{threads: ""} |> build_changeset() |> assert_valid(:threads)
+      %{threads: "1"} |> build_changeset() |> assert_valid(:threads)
+      %{threads: "1,2,3"} |> build_changeset() |> assert_valid(:threads)
+      %{threads: "1-3,5"} |> build_changeset() |> assert_valid(:threads)
+    end
+
+    test "refutes invalid threads values" do
+      %{threads: "5-3"} |> build_changeset() |> assert_invalid(:threads)
+      %{threads: "1, 2"} |> build_changeset() |> assert_invalid(:threads)
+      %{threads: false} |> build_changeset() |> assert_invalid(:threads)
+      %{threads: ["1", "2"]} |> build_changeset() |> assert_invalid(:threads)
+      %{threads: [1, 2]} |> build_changeset() |> assert_invalid(:threads)
+    end
+
+    test "refutes zero and negative allocation values" do
+      %{allocation: 0} |> build_changeset() |> assert_invalid(:allocation)
+      %{allocation: -1} |> build_changeset() |> assert_invalid(:allocation)
+      %{allocation: 1} |> build_changeset() |> assert_valid(:allocation)
+    end
+
+    test "refutes negative memory values" do
+      %{memory: -1} |> build_changeset() |> assert_invalid(:memory)
+      %{memory: 0} |> build_changeset() |> assert_valid(:memory)
+    end
+
+    test "refutes swap values that < -1" do
+      %{swap: -2} |> build_changeset() |> assert_invalid(:swap)
+      %{swap: -1} |> build_changeset() |> assert_valid(:swap)
+      %{swap: 0} |> build_changeset() |> assert_valid(:swap)
+    end
+
+    test "refutes negative io values" do
+      %{io: -1} |> build_changeset() |> assert_invalid(:io)
+      %{io: 0} |> build_changeset() |> assert_valid(:io)
+    end
+
+    test "accepts cpu values only from 0 to 100" do
+      %{cpu: -1} |> build_changeset() |> assert_invalid(:cpu)
+      %{cpu: 0} |> build_changeset() |> assert_valid(:cpu)
+      %{cpu: 50} |> build_changeset() |> assert_valid(:cpu)
+      %{cpu: 100} |> build_changeset() |> assert_valid(:cpu)
+      %{cpu: 101} |> build_changeset() |> assert_invalid(:cpu)
+    end
+
+    test "refutes negative disk values" do
+      %{disk: -1} |> build_changeset() |> assert_invalid(:disk)
+      %{disk: 0} |> build_changeset() |> assert_valid(:disk)
+    end
+  end
+end
---
+++ test/schemas/server/update_startup_params_test.exs
@@ -0,0 +1,63 @@
+defmodule Elidactyl.Schemas.Server.UpdateStartupParamsTest do
+  use ExUnit.Case
+  use Elidactyl.ChangesetCase
+
+  alias Elidactyl.Schemas.Server.UpdateStartupParams
+  import UpdateStartupParams
+
+  @valid %{
+    startup: "java -Xms128M -Xmx{{SERVER_MEMORY}}M -jar {{SERVER_JARFILE}}",
+    environment: %{
+      "SERVER_JARFILE" => "server.jar",
+      "VANILLA_VERSION" => "latest",
+    },
+    egg: 5,
+    image: "quay.io/pterodactyl/core:java",
+    skip_scripts: false
+  }
+
+  def build_changeset(params \\ %{}) do
+    changeset(%UpdateStartupParams{}, Map.merge(@valid, params))
+  end
+
+  describe "changeset/2" do
+    test "allows valid params" do
+      build_changeset() |> assert_valid()
+    end
+
+    test "validates that mandatory fields are present" do
+      changeset = changeset(%UpdateStartupParams{}, %{})
+      assert_invalid(changeset, :startup, error_message == "can't be blank")
+      assert_invalid(changeset, :egg, error_message == "can't be blank")
+      assert_invalid(changeset, :image, error_message == "can't be blank")
+    end
+
+    test "allows optional fields to not present" do
+      %UpdateStartupParams{}
+      |> changeset(Map.drop(@valid, ~w[environment skip_scripts]a))
+      |> assert_valid()
+    end
+
+    test "validates field types" do
+      %{startup: 0} |> build_changeset() |> assert_invalid(:startup)
+      %{environment: "a"} |> build_changeset() |> assert_invalid(:environment)
+      %{egg: "a"} |> build_changeset() |> assert_invalid(:egg)
+      %{image: 0} |> build_changeset() |> assert_invalid(:image)
+      %{skip_scripts: "a"} |> build_changeset() |> assert_invalid(:skip_scripts)
+    end
+
+    test "refutes zero and negative egg values" do
+      %{egg: 0} |> build_changeset() |> assert_invalid(:egg)
+      %{egg: -1} |> build_changeset() |> assert_invalid(:egg)
+      %{egg: 1} |> build_changeset() |> assert_valid(:egg)
+    end
+
+    test "refutes startup values longer than 255 chars" do
+      %{startup: String.pad_leading("", 256, "x")} |> build_changeset() |> assert_invalid(:startup)
+    end
+
+    test "refutes image values longer than 255 chars" do
+      %{image: String.pad_leading("", 256, "x")} |> build_changeset() |> assert_invalid(:image)
+    end
+  end
+end
