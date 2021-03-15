50fe9755b3b0602bda31ac4a06d6cc5c568a6962
Alexey Ovchinnikov <alexey.ovchinnikov@yandex.ru>
2021-03-15 19:49:32 +0300

    feat: get node configuration done

--- test/nodes_test.exs
+++ test/nodes_test.exs
@@ -22,4 +22,29 @@ defmodule Elidactyl.NodesTest do
              }
            ] == allocations
   end
+
+  test "get node configuration" do
+    assert {:ok, configuration} = Nodes.get_configuration(1)
+    assert %{
+      "debug" => false,
+      "uuid" => "1046d1d1-b8ef-4771-82b1-2b5946d33397",
+      "token_id" => "iAcosCn1KCAgVjVO",
+      "token" => "FanPzLCptUxkGow3vi7Z",
+      "api" => %{
+        "host" => "0.0.0.0",
+        "port" => 8080,
+        "ssl" => %{
+          "enabled" => true,
+          "cert" => "/etc/letsencrypt/live/pterodactyl.file.properties/fullchain.pem",
+          "key" => "/etc/letsencrypt/live/pterodactyl.file.properties/privkey.pem",
+        },
+        "upload_limit" => 100,
+      },
+      "system" => %{
+        "data" => "/srv/daemon-data",
+        "sftp" => %{"bind_port" => 2022}
+      },
+      "remote" => "https://pterodactyl.file.properties",
+    } == configuration
+  end
 end
--- test/support/mocked_server/router/applicaiton/nodes.ex
+++ test/support/mocked_server/router/applicaiton/nodes.ex
@@ -46,5 +46,32 @@ defmodule Elidactyl.MockedServer.Router.Application.Nodes do
     end
   end
 
-
+  get "/api/application/nodes/:id/configuration" do
+    if id == "1" do
+      configuration = %{
+        "debug" => false,
+        "uuid" => "1046d1d1-b8ef-4771-82b1-2b5946d33397",
+        "token_id" => "iAcosCn1KCAgVjVO",
+        "token" => "FanPzLCptUxkGow3vi7Z",
+        "api" => %{
+          "host" => "0.0.0.0",
+          "port" => 8080,
+          "ssl" => %{
+            "enabled" => true,
+            "cert" => "/etc/letsencrypt/live/pterodactyl.file.properties/fullchain.pem",
+            "key" => "/etc/letsencrypt/live/pterodactyl.file.properties/privkey.pem",
+          },
+          "upload_limit" => 100,
+        },
+        "system" => %{
+          "data" => "/srv/daemon-data",
+          "sftp" => %{"bind_port" => 2022}
+        },
+        "remote" => "https://pterodactyl.file.properties",
+      }
+      success(conn, configuration)
+    else
+      failure(conn, 404, "not found node #{inspect id}")
+    end
+  end
 end
